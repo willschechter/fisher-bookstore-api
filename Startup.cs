2f04b5d61dce28ad1836f3f37ed8e05fa74bba8d
Will Schechter <Schechter.29@osu.edu>
2020-04-20 16:28:33 -0400

    WillSchechter

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace Fisher.Bookstore
                 options.UseNpgsql(Configuration.GetConnectionString("BookstoreContext"))
             );
             services.AddScoped<IBooksRepository, BooksRepository>();
+            services.AddSingleton<IBooksRepository, TestBooksRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
