8224b483e5803848a0de26d06216476b8e54392c
willschechter <59670788+willschechter@users.noreply.github.com>
2020-05-05 16:23:31 -0400

    Update AuthorsController.cs

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,8 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Fisher.Bookstore.Models;
+using Fisher.Bookstore.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Fisher.Bookstore.Controllers
 {
 
-    public class AuthorsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorsController : ControllerBase 
     {
+        private IAuthorsRepository authorsRepository;
 
+        public AuthorsController(IAuthorsRepository repository)
+        {
+            authorsRepository = repository;
+        }
+        
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+        return Ok();
+        }
     }
 }
