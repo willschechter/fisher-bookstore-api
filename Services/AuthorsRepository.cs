93f417f5b34209081a5e50bfb2053a0860357806
willschechter <59670788+willschechter@users.noreply.github.com>
2020-05-06 21:53:54 -0400

    Update AuthorsRepository.cs

--- Services/AuthorsRepository.cs
+++ Services/AuthorsRepository.cs
@@ -1,8 +1,55 @@
+using System.Collections.Generic;
+using Fisher.Bookstore.Data;
+using Fisher.Bookstore.Models;
+
 namespace Fisher.Bookstore.Services
 {
 
-    public class AuthorsRepository
+    public class AuthorsRepository : IAuthorsRepository
     {
+        private readonly BookstoreContext db;
 
+        public AuthorsRepository(BookstoreContext db)
+        {
+            this.db = db;
+        }
+
+        public int AddAuthor(Author author)
+        {
+            db.Authors.Add(author);
+            db.SaveChanges();
+            return author.Id;
+        }
+
+        public bool AuthorExists(int authorid)
+        {
+            return (db.Authors.Find(authorid) != null);
+
+        }
+
+        public void DeleteAuthor(int authorid)
+        {
+            var author = db.Authors.Find(authorid);
+            db.Authors.Remove(author);
+            db.SaveChanges();
+        }
+
+        public Author GetAuthor(int authorid)
+        {
+            return db.Authors.Find(authorid);
+        }
+
+        public IEnumerable<Author> GetAuthors()
+        {
+            return db.Authors;
+        }
+
+        public void UpdateAuthor(Author author)
+        {
+            var updateAuthor = db.Authors.Find(author.Id);
+            updateAuthor.Name = author.Name;
+            db.Authors.Update(updateAuthor);
+            db.SaveChanges();
+        }
     }
 }
